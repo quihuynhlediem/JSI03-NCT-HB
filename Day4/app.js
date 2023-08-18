42181bde302316b90ed07b965f9226b8fd971a1c
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-08-18 21:34:30 +0700

    Update code Day4

--- Day4/app.js
+++ Day4/app.js
@@ -1,2 +1,21 @@
-let location = document.getElementById('location');
-//fetch(
+let weather = document.getElementById('weather');
+let locationShow = document.getElementById('location_show');
+let temperature = document.getElementById('temperature');
+
+fetch("https://geocoding-api.open-meteo.com/v1/search?name=Hanoi")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    //console.log(data);
+    locationShow.innerHTML = data.results[0].name;
+    let templink = `https://api.open-meteo.com/v1/forecast?latitude=${data.results[1].latitude}&longitude=${data.results[1].longitude}&current_weather=true`;
+    fetch(templink)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        //console.log(data);
+        temperature.innerHTML = data.current_weather.temperature + '°C';
+      })
+  });
