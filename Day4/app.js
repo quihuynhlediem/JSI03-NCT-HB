5a3daca3e8c2c2d00bd46308fe9a243f355b9289
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-08-20 16:39:23 +0700

    Update js code Day4

--- Day4/app.js
+++ Day4/app.js
@@ -1,13 +1,17 @@
 let weather = document.getElementById('weather');
 let locationShow = document.getElementById('location_show');
 let temperature = document.getElementById('temperature');
+let placeSearch = document.getElementById('place_input');
+let placeSearchBtn = document.getElementById('place_input_btn');
 
-fetch("https://geocoding-api.open-meteo.com/v1/search?name=Hanoi")
+placeSearchBtn.addEventListener('click', function () {
+  //console.log(placeSearch.value);
+  fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${placeSearch.value}`)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    //console.log(data);
     locationShow.innerHTML = data.results[0].name;
     let templink = `https://api.open-meteo.com/v1/forecast?latitude=${data.results[1].latitude}&longitude=${data.results[1].longitude}&current_weather=true`;
     fetch(templink)
@@ -19,3 +23,4 @@ fetch("https://geocoding-api.open-meteo.com/v1/search?name=Hanoi")
         temperature.innerHTML = data.current_weather.temperature + '°C';
       })
   });
+ });
