130ab59114d73f4d9794c7e659f5843f0501dff8
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-08-30 13:12:30 +0700

    Test 1 Update Again

--- Test1/app.js
+++ Test1/app.js
@@ -1,48 +1,64 @@
-let timeArray = [[5, 5, 5, 5, 5], [0, 0, 0, 0, 0]];
-//let timeInterval = [];
+let min = [0, 0, 0, 0, 0]
+let second = [0, 0, 0, 0, 0]
+let switchStatus = [0, 0, 0, 0, 0];
+let timer = [];
 
-// Chỉnh từ số sang hiển thị đồng hồ
-function showtimeSet(min, second, t) {
-  if (second == -1) {
-    second = 59;
-    secondShow = String(second);
-    min = min - 1;
-  } else if (second < 10) {
-    secondShow = "0" + String(second);
+for (i = 1; i <= 5; i++) {
+  timer.push(document.getElementById(String(i)));
+}
+
+function showtimeSet(t, timeshow){
+  if (second[t] == 60) {
+    second[t] = 0;
+    secondShow = "0" + String(second[t]);
+    min[t] = min[t] + 1;
+  } else if (second[t] < 10) {
+    secondShow = "0" + String(second[t]);
   } else {
-    secondShow = String(second);
+    secondShow = String(second[t]);
   }
 
-  if (min < 10) {
-    minShow = "0" + String(min);
+  if (min[t] < 10) {
+    minShow = "0" + String(min[t]);
   } else {
-    minShow = String(min);
+    minShow = String(min[t]);
   }
   timeshow.innerHTML = minShow + ' : ' + secondShow;
-  timeArray[0][t] = min;
-  timeArray[1][t] = second;
 }
 
 function startFunction() {
   //console.log(this.event.target.previousElementSibling.firstElementChild);
   let thisTimer = this.event.target.previousElementSibling.firstElementChild;
-  //min = timeArray[0][Number(thisTimer.id) - 1];
-  //second = timeArray[1][Number(thisTimer.id) - 1];
-  timeshow = thisTimer;
-  //console.log(min, second);
-  timeInterval = setInterval(function(){
-      timeArray[1][Number(thisTimer.id) - 1] = timeArray[1][Number(thisTimer.id) - 1] - 1;
-      showtimeSet(timeArray[0][Number(thisTimer.id) - 1], timeArray[1][Number(thisTimer.id) - 1]);
-    }, 1000);
+  //timeshow = thisTimer;
+  switchStatus[Number(thisTimer.id) - 1] = 1;
+
 }
  
 function stopFunction() {
   let thisTimer = this.event.target.previousElementSibling.previousElementSibling.firstElementChild;
-  timeshow = thisTimer;
-  clearInterval(timeInterval);
-  //min = 0;
-  //second = 0;
-  showtimeSet(timeArray[0][Number(thisTimer.id) - 1], timeArray[1][Number(thisTimer.id) - 1], Number(thisTimer.id) - 1);
+  //timeshow = thisTimer;
+  switchStatus[Number(thisTimer.id) - 1] = 0;
+  min[Number(thisTimer.id) - 1] = 0;
+  second[Number(thisTimer.id) - 1] = 0;
+  showtimeSet(Number(thisTimer.id) - 1,timer[Number(thisTimer.id) - 1])
 }
+
+setInterval(function () {
+  for (i = 0; i <= 4; i++) {
+    if (switchStatus[i] == 1) {
+      second[i] = second[i] + 1;
+      showtimeSet(i, timer[i]);
+     }
+  }
+  //console.log(min);
+  //console.log(second);
+}, 1000);
  
-//console.log(timeInterval);
+function stopAllFunction() {
+  for (i = 0; i <= 4; i++) {
+    switchStatus[i] = 0;
+    min[i] = 0;
+    second[i] = 0;
+    showtimeSet(i, timer[i]);
+   }
+ }
