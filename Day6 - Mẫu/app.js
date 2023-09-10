13f8acf1bb1f6912fcfe827bcf9a51c459a6685b
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-09-10 13:03:05 +0700

    Update code for firebase

--- Day6 - Mẫu/app.js
+++ Day6 - Mẫu/app.js
@@ -1,52 +1,46 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, get, child } from "firebase/database";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import {
+	getDatabase,
+	ref,
+	set,
+	get,
+	child,
+} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
 
 const firebaseConfig = {
-      apiKey: "AIzaSyC0i59DC02EOl972GDt-xhtAypbPnFX0XY",
-      authDomain: "jsi03-59bf0.firebaseapp.com",
-      databaseURL: "https://jsi03-59bf0-default-rtdb.firebaseio.com",
-      projectId: "jsi03-59bf0",
-      storageBucket: "jsi03-59bf0.appspot.com",
-      messagingSenderId: "641607578160",
-      appId: "1:641607578160:web:ff69a6fbf2157cbc0173f5",
-      measurementId: "G-6TNZ1JB05C"
+	apiKey: "AIzaSyC4lxHH8YG0nfxndpup7IVzIdA-x-6090Y",
+	authDomain: "jsi03-myfirstproject.firebaseapp.com",
+	projectId: "jsi03-myfirstproject",
+	storageBucket: "jsi03-myfirstproject.appspot.com",
+	messagingSenderId: "633830080267",
+	appId: "1:633830080267:web:36fc363a46947f9c51ec0e",
+	measurementId: "G-0E2QMWM8QV",
 };
-    
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    // const database = getDatabase(app);
 
-    // function writeUserData(userId, name, age) {
-    //   set(ref(database, 'users/' + userId)), {
-    //     name: name,
-    //     age: age
-    //   }
-    // }
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+var rollV, nameV, genderV, addressV;
 
-    // writeUserData("1", 'Linh', 18)
-    const dbRef = ref(getDatabase(app));
-    const userId = "One";
-    get(child(dbRef, `users/${userId}`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
+function readFom() {
+	rollV = document.getElementById("roll").value;
+	nameV = document.getElementById("name").value;
+	genderV = document.getElementById("gender").value;
+	addressV = document.getElementById("address").value;
+	console.log(rollV, nameV, addressV, genderV);
+}
 
-// function readFom() {
-//   rollV = document.getElementById("roll").value;
-//   nameV = document.getElementById("name").value;
-//   genderV = document.getElementById("gender").value;
-//   addressV = document.getElementById("address").value;
-//   console.log(rollV, nameV, addressV, genderV);
-// }
+let createBtn = document.getElementById("create");
+let readBtn = document.getElementById("read");
+let updateBtn = document.getElementById("update");
+let deleteBtn = document.getElementById("delete");
 
-// let createBtn = document.getElementById("create");
-// let readBtn = document.getElementById("read");
-// let updateBtn = document.getElementById("update");
-// let deleteBtn = document.getElementById("delete");
-
-// createBtn.addEventListener("click", function () {});
+createBtn.addEventListener("click", function () {
+	readFom();
+	set(ref(database, "students/" + rollV), {
+		student_roll: rollV,
+		student_name: nameV,
+		student_gender: genderV,
+		student_address: addressV,
+	});
+});
