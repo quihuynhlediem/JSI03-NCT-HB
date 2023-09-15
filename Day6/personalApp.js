fbeedc04722c93dbea2ab620deb9f9616e6c9936
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-09-15 15:43:57 +0700

    Update logout code

--- Day6/personalApp.js
+++ Day6/personalApp.js
@@ -1,2 +1,9 @@
 let heading = document.getElementById("personal_name");
 heading.innerHTML = localStorage.getItem("username");
+
+let logoutBtn = document.getElementById("logout");
+logoutBtn.addEventListener("click", function () {
+  localStorage.removeItem("username");
+  localStorage.removeItem("email")
+  location.assign("index.html");
+});
